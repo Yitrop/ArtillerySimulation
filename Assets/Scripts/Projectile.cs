6ca200d9ece3dee3acd382843c0bd7b85a6e760f
feo <new1passwor@gmail.com>
2026-06-02 17:26:02 +0300

    Marker finally has maden

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public float lifetime = 30f;
+    private Vector3 startPosition;
+    private bool hasHit = false;
 
     void Start()
     {
+        startPosition = transform.position;
         Destroy(gameObject, lifetime);
     }
 
+
     void OnCollisionEnter(Collision collision)
     {
+        if (hasHit) return; // защита от двойного вызова
+        hasHit = true;
+
         Debug.Log("Попадание в: " + collision.gameObject.name);
+
+        // проверяем, что это земля
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            float distance = Vector3.Distance(startPosition, transform.position);
+
+            // создаём маркер
+            MarkerManager.Instance.CreateMarker(transform.position, distance);
+        }
+
         Destroy(gameObject, 0.03f);
         enabled = false;
     }
